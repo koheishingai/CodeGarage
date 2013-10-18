b2571e8313119b1fa785b53e92b6d9c524fd3ad0
Kohei Shingai <koheishingai@gmail.com>
2013-10-19 04:14:52 +0900

    3rd Commit

--- public/drawimg.js
+++ public/drawimg.js
@@ -6,6 +6,13 @@ var imgwidth = {};
 var imgheight = {};
 var imgposition = {};
 
+imgname['#logo'] = '1';
+imgurl['1'] = 'https://ssl.gstatic.com/accounts/ui/logo_2x.png';
+imgwidth['1'] = '232';
+imgheight['1'] = '76';
+imgposition['1'] = '50% 50%';
+
+
 var arr1 = [];
 arr1.push(imgname); //0
 arr1.push(imgurl); //1
@@ -16,18 +23,9 @@ arr1.push(imgposition); //4
 var uniarr = [];
 uniarr.push(arr1);
 
-//Get Setting CSS on Loacal
-function ajaxread(filenm){
-  var xhr=null;
-  if (window.XMLHttpRequest)xhr=new XMLHttpRequest();
-  else if(window.ActiveXObject)
-  try {xhr=new ActiveXObject("Msxml2.XMLHTTP");}
-  catch(e){xhr=new ActiveXObject("Microsoft.XMLHTTP");}
-  xhr.open("GET",filenm,false);xhr.send("");eval(xhr.responseText);
-};
+
 
   $.fn.drawimg = function(name){ //background
-        ajaxread('http://codegarage.azurewebsites.net/outimg.js');
      this.css('background', 'url('+uniarr[0][1][uniarr[0][0][name]]+') no-repeat').css('width', uniarr[0][2][uniarr[0][0][name]]).css('height', uniarr[0][3][uniarr[0][0][name]]).css('background-position', uniarr[0][4][uniarr[0][0][name]]);
   };
 })(jQuery);
